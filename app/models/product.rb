c2c96522a1e5ed348b504d69f1fda710b524b06e
Marissa Abao <mabao@rrc.ca>
2025-07-20 22:26:38 -0500

    Add presence validations to Product model for name and ingredients

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,14 @@
 # app/models/product.rb
 class Product < ApplicationRecord
   belongs_to :brand
+  has_many :reviews
   has_many :product_categories
   has_many :categories, through: :product_categories
   has_many :country_products
   has_many :countries, through: :country_products
-  has_many :reviews
+
+  validates :name, presence: true
+  validates :ingredients, presence: true
 end
+
 
