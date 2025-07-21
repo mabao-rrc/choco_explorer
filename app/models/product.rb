f969d64d46565e7b12fcd55308abd8b165dc31d3
Marissa Abao <mabao@rrc.ca>
2025-07-20 22:08:25 -0500

    Add associations in Product model: belongs_to brand, has_many reviews, categories, countries

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 # app/models/product.rb
 class Product < ApplicationRecord
   belongs_to :brand
+  has_many :product_categories
+  has_many :categories, through: :product_categories
+  has_many :country_products
+  has_many :countries, through: :country_products
+  has_many :reviews
 end
+
