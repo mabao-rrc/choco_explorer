9d5979ec1775c1264f40a73133cfb9b9f8c00041
Marissa Abao <mabao@rrc.ca>
2025-07-20 22:33:44 -0500

    Add validations to Review model: presence and rating range

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 # app/models/review.rb
 class Review < ApplicationRecord
   belongs_to :product
+
+  validates :reviewer_name, presence: true
+  validates :rating, presence: true, inclusion: { in: 1..5 }
+  validates :comment, presence: true
 end
+
